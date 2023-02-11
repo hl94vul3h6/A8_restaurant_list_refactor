dcfe2d94b5d704d10a251b650fa8188ee9f17d51
hl94vul3h6 <gary6780gary6780@gmail.com>
2023-02-11 12:53:02 -0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const restaurantsData = require("./restaurant.json").results;
-const mongoose = require('mongoose')
-const Restaurant = require('./models/restaurant')
 // 引用 body-parser
 const bodyParser = require('body-parser')
 const methodOverride = require("method-override");
@@ -15,22 +12,9 @@ if(process.env.NODE_ENV !== 'production') {
 }
 
 const app = express();
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 const port = 3000;
 
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require("./config/mongoose");
 
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: '.hbs' }));
 app.set("view engine", "hbs");
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,19 +1,11 @@
-const mongoose = require("mongoose");
 const Restaurant = require("../restaurant"); // 載入 restaurant model
 const restaurantList = require("../../restaurant.json").results
+const db = require("../../config/mongoose");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on("error", () => {
-  console.log("mongodb error!");
-});
 db.once("open", () => {
    Restaurant.create(restaurantList)
      .then(() => {
